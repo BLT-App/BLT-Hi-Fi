8bfa256f1f26701f14e7907c56541bbc6f20fb46
dlloydgeorge <32097513+dlloydgeorge@users.noreply.github.com>
2019-11-20 21:41:36 -0500

    Drop down implemented

--- BLT/BLT/NewItemViewController.swift
+++ BLT/BLT/NewItemViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import iOSDropDown
 
-class NewItemViewController: UIViewController, UITextFieldDelegate {
+class NewItemViewController: UIViewController, UITextFieldDelegate  {
 
-    /// Text field for the name of the class.
-    @IBOutlet weak var classText: UITextField!
+    /// Text field for the name of the class
+    @IBOutlet weak var classText: DropDown!
     
     /// Text field for the title of the ToDoItem
     @IBOutlet weak var titleText: UITextField!
@@ -22,27 +23,44 @@ class NewItemViewController: UIViewController, UITextFieldDelegate {
     /// Date picker for the due date.
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    var selected: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        classText.delegate = self
-        titleText.delegate = self
-        descText.delegate = self
+        //classText.delegate = self
+        // The list of array to display. Can be changed dynamically
+        
+        //Its Id Values and its optional
+        //classText.optionIds = [1,23,54,22]
+        globalData.retrieveUserData()
+        
+//        for i in globalData.subjectList {
+//            classText.optionArray.append(i)
+//        }
+        
+        classText.optionArray = globalData.subjectList
+        classText.didSelect { (Selected, index, id) in (self.selected = Selected)
+            
+        }
+        
     }
-    
+
     /// Exits the modal view/screen. 
     @IBAction func exitButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
     /// Checks if the titles are correct, then adds it to the list.
+    //let classTxt = classText.text
     @IBAction func addButton(_ sender: UIButton) {
-        if let classTxt = classText.text, let titleTxt = titleText.text, let descTxt = descText.text {
+        if let titleTxt = titleText.text, let descTxt = descText.text {
             // Debug for clearing/resetting entire list.
             if (titleTxt == "clear_entire_list") {
                 myToDoList.list = []
                 myToDoList.storeList()
-            } else if (classTxt != "" && titleTxt != "") {
-                let newToDo = ToDoItem(className: classTxt, title: titleTxt, description: descTxt, dueDate: datePicker.date, completed: false)
+                
+            }
+            else if (titleTxt != "" && descTxt != "") {
+                let newToDo = ToDoItem(className: selected, title: titleTxt, description: descTxt, dueDate: datePicker.date, completed: false)
                 myToDoList.list.insert(newToDo, at: 0)
                 myToDoList.storeList()
                 
@@ -59,9 +77,10 @@ class NewItemViewController: UIViewController, UITextFieldDelegate {
     
     //Tell Text Fields To Close On Hitting Enter
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == self.classText {
-            self.titleText.becomeFirstResponder()
-        } else if textField == self.titleText {
+//        if textField == self.classText {
+//            self.titleText.becomeFirstResponder()
+//        }
+        if textField == self.titleText {
             self.descText.becomeFirstResponder()
         } else {
             self.view.endEditing(true)
