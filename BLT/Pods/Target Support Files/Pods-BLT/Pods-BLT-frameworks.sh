3309bf589ab451270c1f8f8fb56038d1d47be673
Jiahua Chen <jc@jh.tc>
2019-11-23 10:17:44 -0500

    Cleaned up master
    Cleaned up master after Detail View and Timer View merge. Removed some references to the iOSDropDown, we're using DropDown instead with custom rows.

--- BLT/Pods/Target Support Files/Pods-BLT/Pods-BLT-frameworks.sh
+++ BLT/Pods/Target Support Files/Pods-BLT/Pods-BLT-frameworks.sh
@@ -161,10 +161,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/iOSDropDown/iOSDropDown.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DropDown/DropDown.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/iOSDropDown/iOSDropDown.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DropDown/DropDown.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
